d87011bf0b6c3fb777ee1942e6735e3cfadca1b4
MohammadReza0044 <you@example.com>
2022-11-15 21:26:17 +0330

    member_creat is done

--- library/urls.py
+++ library/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Book_list , search , index
+from .views import Book_list , search , index , member_create
 
 urlpatterns = [
     path('', index , name= 'index'),
     path('books/', Book_list.as_view() , name= 'book_list'),
+    path('member_create/' , member_create , name= 'member_create'),
     path('search/', search , name= 'search'),
 ]
--- library/views.py
+++ library/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render , redirect
 from django.views import View
+from django.contrib import messages
 
 from .models import Book
+from .forms import MemberForm
 
 def index(request):
     return render (request, 'library/index.html')
@@ -12,6 +14,21 @@ class Book_list(View):
     def get (self , request):
         book = Book.objects.all()
         return render (request, 'library/book_list.html', {'book':book})
+
+
+def member_create(request):
+    form = MemberForm(request.POST)
+    if form.is_valid():
+        name = form.cleaned_data['name'] 
+        family = form.cleaned_data ['family'] 
+        phone_number = form.cleaned_data ['phone_number'] 
+        address = form.cleaned_data ['address'] 
+        form.save()
+        messages.add_message(request, messages.SUCCESS, ' با موفقیت ثبت شد')
+        return redirect('/member_create/')
+    else:
+        form = MemberForm
+        return render(request,'library/member_create.html', {'form':form})
 
 
 
